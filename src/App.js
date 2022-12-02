9162cc80d01375d9afef2bbf836aefdd80837d2a
Vineet Rawat <87371365+vineet-codes256@users.noreply.github.com>
2022-12-02 21:56:48 +0530

    updated fetch url

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   useInterval(() => {
     async function fetchData() {
       try {
-        const response = await fetch('http://localhost:8080/getweather');
+        const response = await fetch('http://weatherapi-env.eba-5nj22bbv.ap-northeast-1.elasticbeanstalk.com');
         const json = await response.json();
         hideLoader();
         setData(json);
